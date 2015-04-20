a74cbdc7252baf0c399c7f856885d1126b4516a1
Specialistvlad <Specialistvlad@gmail.com>
2015-04-20 15:19:03 +0300

    googleapis + http server

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ var jwtClient = new google.auth.JWT(
     null,
     ['https://www.googleapis.com/auth/calendar']);
 
-var pathParams = { calendarId: 'stoneleaf.test@gmail.com' };
+var calendarId = 'stoneleaf.test@gmail.com';
 var bodyParams = {
-    id: 'my-unique-id-00001',
+    id: 'my-unique-id-00000220001',
     type: 'web_hook',
-    address: 'https://secure-mountain-3276.herokuapp.com/hook'
+    address: 'https://secure-mountain-3276.herokuapp.com/hook',
+    params: {
+        "ttl": "3000"
+    }
 };
 
 var notifications = {list:[], hooks:[]};
@@ -38,36 +41,12 @@ function getEvents(auth, callback) {
     });
 }
 
-function watchEvents(auth, callback) {
-    var request = require('request');
-    var options = {
-        method: 'POST',
-        uri: 'https://www.googleapis.com/calendar/v3/calendars/stoneleaf.test%40gmail.com/events/watch',
-        headers: {
-            'Authorization': 'Bearer '+auth,
-            'content-type': 'application/json'
-        },
-        json: {
-            id: 'my-unique-id-00000220001',
-            type: 'web_hook',
-            address: 'https://secure-mountain-3276.herokuapp.com/hook',
-            "params": {
-                "ttl": "3000"
-            }
-        }
-    };
-    request(options,
-        function (error, response, body) {
-            callback(error, response, body);
-        });
-}
-
 jwtClient.authorize(function(err, tokens) {
     if (err) {
         console.log(err);
         return;
     }
-    console.log('Auth tokens:', tokens);
+    console.log('Auth success');
 
     /*getEvents(jwtClient, function callback(err, response) {
         if (err) {
@@ -88,81 +67,49 @@ jwtClient.authorize(function(err, tokens) {
         }
     });*/
 
-    watchEvents(tokens.access_token, function callback(error, response, body) {
+    /*watchEvents(tokens.access_token, function callback(error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body); // Show the HTML for the Google homepage.
         } else
             console.log(error, response.statusCode, body);
-    });
+    });*/
 
-    /*calendar.events.watch({
+    calendar.events.watch({
         auth: jwtClient,
-        calendarId: 'stoneleaf.test@gmail.com',
-        id: 'my-unique-id-00000220001',
-        token: "my_token",
-        type: 'web_hook',
-        address: 'https://secure-mountain-3276.herokuapp.com/hook',
-        "params": {
-            "ttl": "3000"
-        }
-
+        calendarId: calendarId,
+        resource: bodyParams
     }, function(err, res) {
         if (err) {
             console.log(err);
             return;
         }
         console.log('Calendar.events.watch:', res);
-    });*/
+    });
 });
 
 
-//*****************************************
-//         Temporary web server
-//*****************************************
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var http = require("http");
 
-// parse application/json
-app.use(bodyParser.raw({limit : 5000}));
-// parse application/json
-app.use(bodyParser.json());
-// parse application/vnd.api+json as json with limit ~5Mb
-app.use(bodyParser.json({type : 'application/vnd.api+json', limit : 5000}));
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended : true}));
+function onRequest(request, response) {
+    var url = require("url").parse(request.url);
+    if (url.path === '/hook') {
+        console.log("Request for " + url.pathname + " received. Result: 200");
+        console.log("headers:",request.headers);
+        console.log("body:",request.body);
+        notifications.list.push({"headers:": request.headers, "body:": request.body});
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.write('');
+    } else if (url.path === '/result') {
+        console.log("Request for " + url.pathname + " received. Result: 200");
+        response.writeHead(200, {"Content-Type": "text/json"});
+        response.write(JSON.stringify(notifications));
+    } else {
+        console.log("Request for " + url.pathname + " received. Result: 404");
+        response.writeHead(404, {"Content-Type": "text/json"});
+        response.write(JSON.stringify({Error: "Not found"}));
+    }
+    response.end();
+}
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-    console.log(req.body);
-    res.status(400).send({error: "Тут ничего нет, сорри :)"});
-});
-
-app.get(['/results','/result'], function(req, res) {
-    res.status(200).send(JSON.stringify(notifications));
-});
-
-app.post('/hook', function(req, res) {
-    console.log({header: req.headers, body:req.body});
-    notifications.list.push({header: req.headers, body:req.body});
-    res.status(200).send('');
-});
-
-app.use(function (req, res, next) {
-    res.status(404);
-    console.log('Not found URL: %s', req.url);
-
-    res.send({error : 'Not found'});
-});
-
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    console.log('Internal error(%d): %s', res.statusCode, err.toString());
-    res.send({error : err.toString()});
-});
-
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'));
-});
+http.createServer(onRequest).listen(process.env.PORT || 5000);
+console.log("Server has started:", process.env.PORT || 5000);
